2baef4221d02c9e4ca435f0feb34cc489ee4e762
Calil-Silva <ka.renner@hotmail.com>
2021-09-08 14:48:02 -0300

    layout da rota historico implementado

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,12 +5,13 @@ import Register from "./Register/register";
 import Habits from "./Habits/habits";
 import CreatedHabits from "./Habits/createdHabits";
 import Today from "./Today/today";
+import History from "./History/history";
 
 export default function App() {
   return (
     <BrowserRouter>
     <Switch>
-      <Route path='/' exact component={Today} />
+      <Route path='/' exact component={History} />
     </Switch>
     </BrowserRouter>
   );
