b8a324183d0c179bdab7c5eb91bf3a2026f2c906
Sidney Outeiro <sid@dcc.ufrj.br>
2021-09-19 07:39:15 -0300

    preparando para o review

--- src/Shared/Components/spinner/loader.js
+++
@@ -1,7 +0,0 @@
-import Loader from "react-loader-spinner";
-
-export default function LoaderSpinner() {
-    return (
-        <Loader type="ThreeDots" color="#fff" height={80} width={80} />
-    )
-}
